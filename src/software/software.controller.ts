cbbae8c8b1380e63b58f154354ffd043812c64c8
KilianAlves <alveskilian@gmail.com>
2023-11-17 14:18:55 +0000

    modification de la requete

--- src/software/software.controller.ts
+++ src/software/software.controller.ts
@@ -4,7 +4,12 @@ import {softwareCollection} from "./software.collection";
 
 export class SoftwareController {
     static async list(req: Request, res: Response, next: NextFunction): Promise<void> {
-        const softwares = await softwareCollection.find({}).limit(20).toArray();
+        const softwares = await softwareCollection.find({}).project({
+            id: 1,
+            name: 1,
+            website: 1,
+            description: 1
+        }).limit(20).toArray();
 
         softwares.forEach((software: any) => {
             console.log(software);
