d714947dd3f852b284cd80a3d9a696855dff8637
KilianAlves <alveskilian@gmail.com>
2023-12-22 16:15:22 +0000

    Mise en cache de données a finir

---
+++ dist/users/users.data.access.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserDataAccess = void 0;
+class UserDataAccess {
+    static async getSoftwareUsers(softwareId) {
+        // Add a return statement here
+        return { users: [], name: "" };
+    }
+}
+exports.UserDataAccess = UserDataAccess;
---
+++ src/users/users.data.access.ts
@@ -0,0 +1,6 @@
+export class UserDataAccess {
+    public static async getSoftwareUsers(softwareId: number): Promise<{users: SoftwareUsers[], name: string}> {
+        // Add a return statement here
+        return { users: [], name: "" };
+    }
+}
